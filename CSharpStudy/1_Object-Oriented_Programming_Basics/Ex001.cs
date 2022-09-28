dc7c43dbdbbcf41b988aafaf3ca31c71d70f7610
mungkeu <101312446+mungkeu@users.noreply.github.com>
2022-09-28 23:36:52 +0900

    Initial commit

---
+++ CSharpStudy/1_Object-Oriented_Programming_Basics/Ex001.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy._1_Object_Oriented_Programming_Basics
+{
+    public class Ex001_Product
+    {
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        public Ex001_Product(int code, string name, int price)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public int GetTax()
+        {
+            return (int)(Price * 0.08);
+        }
+        public int GetPriceIncludingTax()
+        {
+            return Price + GetTax();
+        }
+    }
+    internal class Ex001
+    {
+        public void Run()
+        {
+            Ex001_Product yakkwa = new Ex001_Product(123, "약과", 180);
+            int price = yakkwa.Price;
+            int taxIncluded = yakkwa.GetPriceIncludingTax();
+            Ex001_Product chapssal = new Ex001_Product(235, "찹쌀떡", 160);
+        }
+    }
+}
---
+++ CSharpStudy/1_Object-Oriented_Programming_Basics/Ex002.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy._1_Object_Oriented_Programming_Basics
+{
+    // 클래스 참조형
+    class Ex002ClassPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    // 구조체 값형
+    struct Ex002StructPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+        public Ex002StructPoint(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    internal class Ex002
+    {
+        public void Run()
+        {
+            // c#에 정의되어 있는 구조체
+            DateTime date = new DateTime(2022, 9, 28);
+            int year = date.Year;
+            DateTime daysAfter10 = date.AddDays(10); // 10일 후 
+
+            // 구조체 테스트
+            Ex002StructPoint a = new Ex002StructPoint(10, 20);
+            Ex002StructPoint b = a;
+            Console.WriteLine("a:({0},{1})",a.X,a.Y);
+            Console.WriteLine("b:({0},{1})", b.X, b.Y);
+            a.X = 80;
+            Console.WriteLine("a:({0},{1})", a.X, a.Y);
+            Console.WriteLine("b:({0},{1})", b.X, b.Y);
+        }
+    }
+}
